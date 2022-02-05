800965c636678b58eda2597907823fc5f3d89064
Dylan <dyl.blink.1.8.2@gmail.com>
2022-02-05 13:07:20 -0600

    ciclo de vida useEffect

--- modulo3/week2/saba/ciclo-de-vida/src/components/List.js
+++ modulo3/week2/saba/ciclo-de-vida/src/components/List.js
@@ -1,20 +1,38 @@
 import { useEffect,useState } from "react";
 import axios from 'axios';
 function  List(){
-    let list = {};
-    let lis2 = [
-        {name:"Mango"},
-        {name:"Jax"},
-        {name:"Kevin"},
-        {name:"Melvin"},
-        {name:"Jodie"},
-        {name:"Kikis"}
-    ];
 
+
+    const [list,setList] = useState([])
+
+
+
+    const getCharacter = async () =>{
+        try{
+            const { data } = await axios.get("https://rickandmortyapi.com/api/character");
+            setList(data.results)
+            console.log("data",data)
+        }catch(e){
+            console.log("el error",e)
+        }
+    }
+    //cuando el [] estan vacion solo se va a ejecutar una sola vez!!!
+    useEffect(()=>{
+        // codigo de stackoverflow
+        // async function perro(){
+        //      await perro2.get()
+        // }
+        // perro()
+
+        getCharacter()
+    },[])
     return(
         <div>
+            <span>Esta es una lista!</span>
             {
-                lis2.map((perro,index)=>(<p key={index}>{perro.name}</p> ))
+                //list.length === 0(null,undefined) || list.lenght == 0  || false
+                !list.length ? <p>Estoy cargando la lista</p> :
+                list.map((perro,index)=>(<p key={index}>{perro.name}</p> ))
             }
             {/* {
                 lis2.map((perro,item)=>{
